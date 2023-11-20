6ea353119c5aebd7b6398bb428ef696763d2c764
vharatyk <haratyk2005@gmail.com>
2023-11-20 23:18:36 +0100

    not verry fast

---
+++ client.c
@@ -0,0 +1,85 @@
+#include<signal.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+
+// check PID 
+static int is_nubers(char *argv)
+{
+int i;
+
+i = 0;
+while(argv[i])
+{
+if(argv[i] <= '1' && argv[i] >= '9')
+    i++ ;
+else 
+    return(0);
+}
+return(1);
+}
+
+// check the value enter for user 
+static int good_argument(int argc , char **argv)
+{
+    if(argc != 3)
+    {
+        printf("ERROR : <PID> <MSG>");
+        return(0);
+    }
+    else if(is_nubers(argv[1]))
+    {
+        printf("ERROR : PID = INT");
+        return(0);
+    }
+    else if(argv[2] == 0)
+    {
+        printf("KO : NO MESSAGE" );
+        return(0);
+    }
+    else 
+        return(1);
+}
+
+// envoyer des bits 
+static void send_octet(char c , int pid )
+{
+int bit;
+
+bit = 7 ;
+while(bit >= 0) 
+    {
+        if( c >> bit & 1)
+            kill(pid , SIGUSR1);
+        else 
+            kill(pid , SIGUSR2);
+        usleep(200);
+        bit-- ;
+    }
+
+
+}
+
+
+int main (int argc ,char** argv)
+{
+    int pid;
+    int i;
+    char    *str;    
+
+    if (good_argument(argc , argv ) == 1)
+    {
+            
+        pid = atoi(argv[1]);
+        str = argv[2];
+        i = 0;
+
+        while(str[i])
+        {
+            send_octet(str[i++] , pid );
+        }
+        //free(str);
+        send_octet('\n', pid);
+        return 0 ;
+    }
+}
--- serveur.c
+++ serveur.c
@@ -1,23 +1,42 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   serveur.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: vharatyk <vharatyk@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/17 12:00:55 by vharatyk          #+#    #+#             */
-/*   Updated: 2023/11/17 12:08:07 by vharatyk         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+
+
+#include<unistd.h>
+#include<stdio.h>
+#include<signal.h>
 
 
 
-
-int main (void)
+void convert_message (int signal)
 {
-while(1)
-{
-sleep(1);
+    static int bit = 0; 
+    static int c ;
+
+
+    if(signal == SIGUSR1)
+        c += 1 <<(7 - bit);
+    bit++ ;
+
+    if(bit == 8)
+    {
+        write(1 ,&c ,1);
+        bit = 0;
+        c = 0;
+    }
 }
-return 0 ;
-}
+
+
+int main (int argc , char **argv)
+    {
+        if(argc != 1 )
+        {
+            printf(" ERROR :IS SERVOR");
+            return(0);
+        }
+    printf("PID : %i \n", getpid());
+    signal(SIGUSR1 ,convert_message );
+    signal(SIGUSR2 ,convert_message );
+    while(1)
+        usleep(200);
+
+    return(0);
+    }
